48cf7be16b3a0c7bf8ed012a0f6bb90497ff7b79
at-oanhle <oanh.le@asiantech.vn>
2018-01-29 09:40:01 +0700

    first commit

---
+++ target/adapter/JavaScriptDemoAPI-impl.js
@@ -0,0 +1,46 @@
+/*
+ *  Licensed Materials - Property of IBM
+ *  5725-I43 (C) Copyright IBM Corp. 2011, 2016. All Rights Reserved.
+ *  US Government Users Restricted Rights - Use, duplication or
+ *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+/**
+ * @param tag: a topic such as MobileFirst_Platform, Bluemix, Cordova.
+ * @returns json list of items.
+
+ */
+
+
+function showLoca(location) {
+  var input = {
+    method : 'get',
+    path : '/data/2.5/weather',
+    returnedContentType : 'json',
+    parameters: {
+      "appid": "b6907d289e10d714a6e88b30761fae22",
+      "id": '2172797'
+    }
+  };
+  var response = MFP.Server.invokeHttp(input);
+  return { result: response };
+}
+
+
+function getFeed() {
+  var input = {
+      method : 'get',
+      returnedContentType : 'xml',
+      path : "feed.xml"
+  };
+
+  return MFP.Server.invokeHttp(input);
+}
+
+function getAuthUserId(){
+   var securityContext = MFP.Server.getTokenIntrospectionData();
+   var user = securityContext.getAuthenticatedUser();
+
+   return "User ID: " + user.getId;
+}
+
